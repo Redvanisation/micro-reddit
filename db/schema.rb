f4ed5492d329206167f23e81fd9a8c8ee0eaf68d
Redvan <redvanisation@gmail.com>
2019-08-29 10:32:40 -0400

    Creating the comment model with validation and testing the database associations

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,18 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_08_29_132136) do
+ActiveRecord::Schema.define(version: 2019_08_29_135743) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "name"
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.integer "post_id"
+    t.integer "user_id"
+    t.index ["post_id"], name: "index_comments_on_post_id"
+    t.index ["user_id"], name: "index_comments_on_user_id"
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "url"
