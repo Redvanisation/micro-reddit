6034c414e49e963c19affd5c4b494ec9955a6fdc
Mouha Diouf <mouhamadoudioufsn@gmail.com>
2019-08-29 14:40:48 +0000

    Saving changes

---
+++ app/models/post.rb
@@ -0,0 +1,6 @@
+class Post < ApplicationRecord
+    has_many :comments
+    belongs_to :user
+    validates :url, presence: true
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,6 @@
+class User < ApplicationRecord
+    has_many :posts
+    has_many :comments
+    validates :name, presence: true, length: {maximum: 12}, uniqueness: true
+
+end
